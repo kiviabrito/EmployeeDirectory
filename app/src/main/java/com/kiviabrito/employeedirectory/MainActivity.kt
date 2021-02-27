d26143e9c30f33c452a9b4babe930c4400d5d304
Kivia Brito <kivia.brito12@gmail.com>
2021-02-27 10:16:31 -0700

    first commit

---
+++ app/src/main/java/com/kiviabrito/employeedirectory/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kiviabrito.employeedirectory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kiviabrito.employeedirectory.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
